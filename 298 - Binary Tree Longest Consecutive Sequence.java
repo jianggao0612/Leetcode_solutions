c14b83c13b87349be152b299a240c95882fbf78a
Gao Jiang <gaoj@andrew.cmu.edu>
2015-10-28 16:05:39 -0400

    298 - Binary Tree Longest Consecutive Sequence

--- 298 - Binary Tree Longest Consecutive Sequence.java
+++ 298 - Binary Tree Longest Consecutive Sequence.java
@@ -5,6 +5,8 @@
  * The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. 
  * The longest consecutive path need to be from parent to child (cannot be the reverse).
  *
+ * Recursive implementation
+ *
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -14,7 +16,34 @@
  * }
  */
 public class Solution {
+
+	int max = 0;
+    
     public int longestConsecutive(TreeNode root) {
-        
+    	
+    	if (root == null)
+    		return 0;
+    	longestConsecutiveCount(null, root, 0);
+    	return max;
+
+
     }
+
+    public void longestConsecutiveCount(TreeNode pre, TreeNode cur, int count) {
+    	if (cur == null)
+    		return;
+
+    	if ((pre == null) || ((cur.val - pre.val) != 1))
+    		count = 1;
+    	else{
+    		count ++;
+    	}
+
+    	if (max < count)
+    		max = count;
+
+    	longestConsecutiveCount(cur, cur.left, count);
+    	longestConsecutiveCount(cur, cur.right, count);
+    }
+
 }
