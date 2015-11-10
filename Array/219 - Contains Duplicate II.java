fb8fe042602f24a28e633041059a399d3fa3aa40
Gao Jiang <gaoj@andrew.cmu.edu>
2015-11-10 18:53:28 -0500

    update array easy 219 to be the correct one

--- Array/219 - Contains Duplicate II.java
+++ Array/219 - Contains Duplicate II.java
@@ -8,34 +8,23 @@ public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
 
         if (nums.length <= 1) {
-
             return false;
-
         } 
 
-        if (k > (nums.length - 1)) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(); // use a hashmap to record and update the position of each element
 
-            return false;
+        for(int i = 0; i < nums.length; i++) {
 
-        }
-
-        HashSet<Integer> visited = new HashSet<Integer>();
-
-        int i = 0, j = i + k;
-
-        while (j < nums.length) {
-
-            visited.add(nums[i]);
-
-            if (visited.contains(nums[j])) {
-
-                return true;
-
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
             } else {
 
-                i++;
-                j++;
-
+                if ((i - map.get(nums[i])) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i); // update the position to the new postion
+                }
+                
             }
         }
 
